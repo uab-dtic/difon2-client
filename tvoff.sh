42a6515b8a62e9f65d0ca34d899b7640bc832bfc
Adolfo Amo <adolfo.amo@uab.cat>
2023-11-10 12:14:22 +0100

    Afegits scripts de control de pantalla

---
+++ tvoff.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "standby 0" | cec-client -s > /dev/null
+echo "standby" > /var/lib/pantalles/tvstatus
+killall chromium-browser
+#Apaguem HDMI
+xset -display :0.0 dpms force off
---
+++ tvon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Encenem HDMI
+xset -display :0.0 dpms force on
+echo "on 0" | cec-client -s > /dev/null
+echo "on" > /var/lib/pantalles/tvstatus
+# /home/pi/kiosk.sh&
