f8d7a3489eb3fef17ed68cd4bda2b58bead32ea9
Adolfo Amo <adolfo.amo@uab.cat>
2023-12-21 15:12:01 +0100

    Mirem si el kiosk ja està en marxa

--- tvon.sh
+++ tvon.sh
@@ -3,4 +3,9 @@
 xset -display :0.0 dpms force on
 echo "on 0" | cec-client -s > /dev/null
 echo "on" > /var/lib/pantalles/tvstatus
-/home/pi/scripts/kiosk.sh&
+
+CHROMIUM_INSTS=`ps -ef | grep chromium-browser | wc -l`
+
+if [ "$CHROMIUM_INSTS" -eq 1 ]; then
+  /home/pi/scripts/kiosk.sh&
+fi
