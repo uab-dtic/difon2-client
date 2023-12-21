ccf361365e97e756e19515f3b8b7807729cdd8bc
Adolfo Amo <adolfo.amo@uab.cat>
2023-12-21 18:00:27 +0100

    canviada la gestio per bits

--- functions.sh
+++ functions.sh
@@ -2,60 +2,57 @@
 
 procesa_status() {
 
-case $1 in
+  ACCIO_PC=${1:0:1}
+  ACCIO_PANT=${1:1:2}
 
-  00)
+case $ACCIO_PC in
+
+  0)
     echo "No hago nada"
     logger "DIFON: $0: No faig res"
   ;;
 
-  10)
+  1)
     #echo "Apagar Client"
     logger "DIFON: $0: Apagar Client"
     sudo poweroff
   ;;
 
-  20)
+  2)
     echo "Reiniciar Cliente"
     logger "DIFON: $0: Reiniciar PC"
     sudo reboot
   ;;
-
-  01)
-    echo "Apagar Pantalla"
-    logger "DIFON: $0: Apagar Pantalla"
-    /home/pi/scripts/tvoff.sh
+  8)
+    case $ACCIO_PANT in
+      0)
+          echo "Error data buida"
+          logger "DIFON: $0: Error data buida"
+      ;;
+      1)
+          echo "Error en la data rebuda"
+          logger "DIFON: $0: Error en la data rebuda"
+      ;;
+    esac
+    exit 0
   ;;
 
-  02)
-    echo "Encendre Pantalla"
-    logger "DIFON: $0: Encendre Pantalla"
-    /home/pi/scripts/tvon.sh
-  ;;
-
-  80)
-    echo "Error data buida"
-    logger "DIFON: $0: Error data buida"
-  ;;
-
-  81)
-    echo "Error en la data rebuda"
-    logger "DIFON: $0: Error en la data rebuda"
-  ;;
-
-  90)
-    echo "Error MAC buida"
-    logger "DIFON: $0: Error MAC buida"
-  ;;
-
-  91)
-    echo "Error en la MAC rebuda"
-    logger "DIFON: $0: Error en la MAC rebuda"
-  ;;
-
-  92)
-    echo "Error MAC no trobada a la BBDD"
-    logger "DIFON: $0: Error MAC no trobada a la BBDD"
+  9)
+    case $ACCIO_PANT in
+      0)
+          echo "Error MAC buida"
+          logger "DIFON: $0: Error MAC buida"
+      ;;
+      1)
+          echo "Error en la MAC rebuda"
+          logger "DIFON: $0: Error en la MAC rebuda"
+      ;;
+      2)
+          echo "Error MAC no trobada a la BBDD"
+          logger "DIFON: $0: Error MAC no trobada a la BBDD"
+      ;;
+    esac
+    exit 0
   ;;
 
   *)
@@ -64,5 +61,20 @@ case $1 in
     exit -2
   ;;
 esac
+
+case $ACCIO_PANT in
+  1)
+    echo "Apagar Pantalla"
+    logger "DIFON: $0: Apagar Pantalla"
+    /home/pi/scripts/tvoff.sh
+  ;;
+ 
+  2)
+    echo "Encendre Pantalla"
+    logger "DIFON: $0: Encendre Pantalla"
+    /home/pi/scripts/tvon.sh
+  ;;
+esac
+
 
 }
