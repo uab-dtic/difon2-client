4755a9f88d3cc67cff9baa941784fbdb0f11826d
Adolfo Amo <adolfo.amo@uab.cat>
2024-03-13 15:40:12 +0100

    Arreglat compatibilitat comanda chromium
    rpi -> chromium-browser
    beelink -> chromium

--- tvoff.sh
+++ tvoff.sh
@@ -2,6 +2,7 @@
 echo "standby 0" | cec-client -s > /dev/null
 echo "standby" > /var/lib/pantalles/tvstatus
 killall chromium-browser
+killall chromium
 #Apaguem HDMI
 #xset -display :0.0 dpms force off
 vcgencmd display_power 0
--- tvon.sh
+++ tvon.sh
@@ -5,7 +5,7 @@ vcgencmd display_power 1
 echo "on 0" | cec-client -s > /dev/null
 echo "on" > /var/lib/pantalles/tvstatus
 
-CHROMIUM_INSTS=`ps -ef | grep chromium-browser | wc -l`
+CHROMIUM_INSTS=`ps -ef | grep chromium | wc -l`
 
 if [ "$CHROMIUM_INSTS" -eq 1 ]; then
   /home/pi/scripts/kiosk.sh&
